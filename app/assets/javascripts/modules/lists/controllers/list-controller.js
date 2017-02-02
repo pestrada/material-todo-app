cb89e2b750188f5cb79ceb5bd7b2f6d916cf8f69
Paul Estrada <paul_e_mtz@hotmail.com>
2017-02-02 00:14:13 -0600

    display form for new list

--- app/assets/javascripts/modules/lists/controllers/list-controller.js
+++ app/assets/javascripts/modules/lists/controllers/list-controller.js
@@ -1,19 +1,38 @@
 angular.module('Lists')
 
-.controller('ListController', ['$scope', '$http', function ($scope, $http) {
+.controller('ListController', ['$scope', '$http', '$mdDialog', function ($scope, $http, $mdDialog) {
   $scope.lists = [];
   $scope.progress = 0;
 
-  $http.get('http://localhost:3000/lists.json').then(function(response) {
-    $scope.lists = response.data;
-  });
+  var loadData = function () {
+    $http.get('http://localhost:3000/lists.json').then(function(response) {
+      $scope.lists = response.data;
+    });
+  };
 
   $scope.listsEmpty = function () {
     return $scope.lists.length == 0;
   };
 
-  $scope.newList = function () {
-    
+  $scope.newList = function (ev) {
+    // Appending dialog to document.body to cover sidenav in docs app
+    var confirm = $mdDialog.prompt()
+      .title('Enter the list name')
+      .textContent('What would you like to acomplish?')
+      .placeholder('learn something new')
+      .ariaLabel('List name')
+      .initialValue('')
+      .targetEvent(ev)
+      .ok('Save')
+      .cancel('Cancel');
+
+    $mdDialog.show(confirm).then(function(result) {
+      loadData()
+    }, function() {
+      
+    });
   };
+
+  loadData();
 }
 ]);
