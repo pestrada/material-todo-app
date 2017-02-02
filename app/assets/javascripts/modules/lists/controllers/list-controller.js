9cd8b31b81053318b4dc4c773b25cf46e2f1f0a7
Paul Estrada <paul_e_mtz@hotmail.com>
2017-02-01 23:44:43 -0600

    display lists as cards

--- app/assets/javascripts/modules/lists/controllers/list-controller.js
+++ app/assets/javascripts/modules/lists/controllers/list-controller.js
@@ -2,9 +2,14 @@ angular.module('Lists')
 
 .controller('ListController', ['$scope', '$http', function ($scope, $http) {
   $scope.lists = [];
+  $scope.progress = 0;
 
   $http.get('http://localhost:3000/lists.json').then(function(response) {
     $scope.lists = response.data;
   });
+
+  $scope.listsEmpty = function () {
+    return $scope.lists.length == 0;
+  };
 }
 ]);
